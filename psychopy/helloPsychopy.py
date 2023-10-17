6967464fa1cc99f268562fb2da08ec80a7bf15df
Otto Kuosmanen <kuosmanen.otto@gmail.com>
2023-10-17 14:29:54 +0200

    Documentation
    adding documentation
    strating to implement psychopy rendering from classes

--- psychopy/helloPsychopy.py
+++ psychopy/helloPsychopy.py
@@ -1,19 +1,52 @@
-from psychopy import visual, core, event, sound, prefs  # import some libraries from PsychoPy
+# IMPORTS
+from psychopy import visual, core, event, sound, prefs
 import random
 import csv
 import os
-prefs.hardware['audioLib'] = ['pygame']
-filename = "../results/data.csv"
+
+# Output
+    # To do, make data file name dependent on the user input.
+    # or generate random code for users, or something along theese lines.
+results_destination = "results/"
+filename = "BLANK"
 logger = []
 
+# CLASSES
 class Card:
-    image_path = "cards/"
+    """
+    A card class that creates playing card objects.
+    Attributes:
+    -----------
+    number : int
+        The number associated with the card
+    shape : str
+        The shape of the card (e.g., "circle", "square").
+    color : str
+        The color of the card
 
+    Methods:
+    --------
+    get_card_property(prop) -> str:
+        Returns the requested property of the card. `prop` can be "number", "shape", or "color".
+
+    get_filename() -> str:
+        Returns the filename of the image associated with the card.
+        _CAUTION_
+        Dependent on correct image_path: Sett inside the class. 
+        As of now, only works if you actually have png files at the image_path, that are named following the format: number_shape_color.png
+        
+    get_psychopy(position) -> obj
+        Creates a PsychoPy ImageStim object representing the card.
+    """
+    
+    #The directory path where card images are stored.
+    image_path = "cards/"
+    
     def __init__(self,number,shape,color):
         self.number = number
         self.shape = shape
         self.color = color
-
+        
     def get_card_property(self, prop):
         """
         Function returns one of the properties of the card.
@@ -29,6 +62,10 @@ class Card:
             raise AttributeError("Unknown atttribute")
     
     def __repr__(self):
+        """
+        Returns the string representation of the card object
+        str: Card(number,shape,color)
+        """
         return "Card({num},{shape},{color})".format(num=self.number,shape=self.shape, color=self.color)
     
     def get_filename(self):
@@ -36,18 +73,53 @@ class Card:
         fname = os.path.join(self.image_path, "%i_%s_%s.png"%(self.number, self.shape, self.color))
         return fname
     
-    def get_psychopy(self):
-        return None
-        #ppy_repr = psychopy.visual.ImageStim(self.get_filename())
-        #return ppy_repr
+    def get_psychopy(self, position=(0,0)):
+        """
+        Creates a PsychoPy ImageStim object representing the card.
+    
+        Parameters:
+        -----------
+        position : tuple of int, optional
+            The (x, y) coordinates for the position of the image in the window.
+            Defaults to (0, 0).
+    
+        Returns:
+        --------
+        A PsychoPy ImageStim object with the card's image set at the specified position.
+        """
+        ppy_repr = visual.ImageStim(win,image=self.get_filename(),size=(card_size),pos=(position))
+        return ppy_repr
             
 class Stack():
+    
+    """
+    A class that simulates a stack, akin to a deck of cards.
+    
+    Attributes:
+    -----------
+    list_of_cards : list[Card1,Card2,Card3]
+        A list containing objects of the Card class.
+        
+    Methods:
+    --------
+    add(new_card: Card) -> None:
+        Adds the given card to the top of the stack.
+    
+    pop() -> Card:
+        Removes and returns the card from the top of the stack.
+    
+        _CAUTION_
+        THe end of the list is conceptualized as the top of the stack
+    
+    render()
+    """
+
+    
     def __init__(self,list_of_cards):
         self.list_of_cards = list_of_cards
         
     def __repr__(self):
         return repr(self.list_of_cards)
-    
     
     def __len__(self):
         return len(self.list_of_cards)
@@ -59,10 +131,8 @@ class Stack():
         return self.list_of_cards.pop()
     
     def render(self):
-        pass
-        #obj = self.list_of_cards[-1].get_psychopy()
-        #obj.x_pos = self.xpos
-        #print(self.list_of_cards[-1].shape,self.list_of_cards[-1].number,self.list_of_cards[-1].color)
+        obj = self.list_of_cards[-1].get_psychopy()
+        obj.x_pos = self.xpos
 
 class MainStack(Stack):
     """
@@ -125,7 +195,7 @@ class DiscardStack(Stack):
         # call self.stimcard.render() and place at ypos_stimcard
         # call self.list_of_cards[-1].render()
 
-
+# FUNCTIONS
 def user_input():
     while True:
         try:
@@ -147,27 +217,28 @@ def matched_category(rules,choice,card):
             matched.append(rule)
     return matched
 
-def save_results(data, filename):
-    with open(filename, 'w', newline='') as csvfile:
+
+
+def save_results(data, results_destination, filename):
+    full_path = os.path.join(results_destination, filename)
+    with open(full_path, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for row in data:
             writer.writerow(row)
 
+# Create stacks of cards
 mainstack = MainStack()
 dstacks = {i:DiscardStack(i) for i in range(1,5)}
 
 # initialize
-
-deck_active = True
 rules = ["shape", "color", "number"]
 active_rule = random.choice(rules)
 win_streak=0
-
-# create a window
+card_size = (128,176)
 win = visual.Window([1600,1200], monitor="testMonitor", units="pix")
 
-#Sett card size
-card_size = (128,176)
+
+# PSYCHOPY CONSTANTS
 
 # get filename of stimuli
 s1 = dstacks[1].stimulus_card.get_filename()
@@ -188,6 +259,16 @@ success = {
     'font': 'Arial',
     'height': 42,
     'color': 'green',
+    'bold': True,
+    'italic': False,
+    'pos': (0, 0)
+}
+
+fail = {
+    'text': 'Wrong!',
+    'font': 'Arial',
+    'height': 42,
+    'color': 'red',
     'bold': True,
     'italic': False,
     'pos': (0, 0)
@@ -235,8 +316,6 @@ stim3_text = visual.TextStim(win, **three)
 stim4_text = visual.TextStim(win, **four)
 
 #Sound
-
-
 # Create a sound object from an audio file
 sound_file = "sounds/win.wav"
 win_music = sound.Sound(sound_file)
@@ -244,10 +323,12 @@ sound.init()
 
 
 # GAMELOOP
-for card in mainstack.list_of_cards:
+while mainstack.list_of_cards:
+    # Take a card
     card = mainstack.pop()
-    hand_img = card.get_filename()
-    hand = visual.ImageStim(win,image=hand_img,size=(card_size),pos=(mainstack.xpos,mainstack.ypos))
+    # Load the hand variable as a visual object
+    hand = visual.ImageStim(win,image=card.get_filename(),size=(card_size),pos=(mainstack.xpos,mainstack.ypos))
+    # Render it on the window
     hand.draw()
     
     stim1.draw()
@@ -269,15 +350,16 @@ for card in mainstack.list_of_cards:
     choice = int(keys[0])
     dstacks[choice].add(card)
     
+    # Render discard piles
     for i, value in enumerate(range(1, 5)):
         if len(dstacks[value].list_of_cards) > 0:
             d = visual.ImageStim(win, image=dstacks[value].list_of_cards[-1].get_filename(), size=(card_size), pos = (dstacks[value].xpos, dstacks[value].ypos_discard))
             d.draw()
     
+    
     chosen_card=dstacks[choice].stimulus_card
     correct = card.get_card_property(active_rule)==chosen_card.get_card_property(active_rule)
     
-    print(correct)
     if correct:
         win_music.stop()
         win_music.play()
@@ -286,6 +368,8 @@ for card in mainstack.list_of_cards:
         text.draw()
     else:
         win_streak = 0
+        text = visual.TextStim(win, **fail)
+        text.draw()
     # Logg results
     match = matched_category(rules, choice, card)
     trial = [active_rule, match]
@@ -295,7 +379,9 @@ for card in mainstack.list_of_cards:
     if win_streak >= 5:
         active_rule=random.choice(list(set(rules).difference([active_rule])))
         win_streak = 0
-    
+        
+print(logger)
+
 # close the window
 win.close()
 
